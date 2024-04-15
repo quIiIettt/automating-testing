506409bc8e575845e2a78c4a5277fff02fe60455
dqie.ee <103836441+quIiIettt@users.noreply.github.com>
2024-04-15 12:29:19 +0300

    add runner

--- prac7/t_waits.py
+++ prac7/t_waits.py
@@ -15,50 +15,37 @@ def browser():
 def get_url(browser): 
     link = "https://testpages.eviltester.com/styled/index.html"
     browser.get(link)
-    browser.find_element(By.XPATH, "//a[contains(text(),'Dynamic Buttons Challenge 02')]").click()
+    browser.find_element(By.XPATH, "//a[contains(text(),'Dynamic Buttons Challenge 01')]").click()
 
 def test_timeSleep(browser):
     get_url(browser)
-
     start_button = browser.find_element(by=By.ID, value='button00')
     start_button.click()
-
     one_button = browser.find_element(by=By.ID, value='button01')
     one_button.click()
     time.sleep(3)
-
     two_button = browser.find_element(by=By.ID, value='button02')
     two_button.click()
-
     assert browser.find_element(by=By.ID, value='button02').is_displayed() 
 
 def test_webDriverWait(browser):
     get_url(browser)
-
     start_button = browser.find_element(by=By.ID, value='button00')
     start_button.click()
-
     one_button = browser.find_element(by=By.ID, value='button01')
     one_button.click()
-
     WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.ID, 'button02')))
-
     two_button = browser.find_element(by=By.ID, value='button02')
     two_button.click()
-
     assert browser.find_element(by=By.ID, value='button02').is_displayed()
 
 def test_implicitlyWait(browser):
     get_url(browser)
-
     start_button = browser.find_element(by=By.ID, value='button00')
     start_button.click()
-
     one_button = browser.find_element(by=By.ID, value='button01')
     one_button.click()
     browser.implicitly_wait(10)
-
     two_button = browser.find_element(by=By.ID, value='button02')
     two_button.click()
-
     assert browser.find_element(by=By.ID, value='button02').is_displayed()
